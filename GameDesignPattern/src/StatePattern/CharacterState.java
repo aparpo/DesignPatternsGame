0545c1e0e7d4e5e1b99a6f85525cf88ed4ae3e00
erickdcs <51513004+erickdcs@users.noreply.github.com>
2021-04-23 17:58:29 +0200

    correccion State Pattern

--- GameDesignPattern/src/StatePattern/CharacterState.java
+++ GameDesignPattern/src/StatePattern/CharacterState.java
@@ -2,8 +2,8 @@ package StatePattern;
 
 public interface CharacterState {
 	public void process();
-	public void standard();
 	public void paralyzed();
 	public void poisoned();
 	public void confused();
+	public void standard();
 }
