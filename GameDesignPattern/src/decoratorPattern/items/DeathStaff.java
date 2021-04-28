56fe5995aaaf9656d7037dc6b070b156aae83c8e
aparpo <alvaro.parpolowicz@live.u-tad.com>
2021-04-28 18:49:23 +0200

    .

--- GameDesignPattern/src/decoratorPattern/items/DeathStaff.java
+++ GameDesignPattern/src/decoratorPattern/items/DeathStaff.java
@@ -7,14 +7,14 @@ import singletonPattern.GameManager;
 public class DeathStaff extends ActiveItemDecorator{ //Mata al enemigo a cambio de ponerte un estado negativo
 
 	public DeathStaff(Item equipment) {
-		super(equipment, "Death Staff", "Kill", 0, 0, 10, 0, 0);
+		super(equipment, "Death Staff", "Kill", 0, 0, 10, 0, 0,ActionType.OFFENSIVE, SkillType.MAGIC);
 	}
 
 	@Override
 	public void useSkill(Character user, Character target) {
 		Stats variation = new Stats(-target.getEquipment().getLife(), 0, 0, 0, 0); //Actualizar vida actual a cero
 		//Cambiar estado a estado negativo
-		GameManager.getManager().getActions().add(new Action(variation, ActionType.OFFENSIVE, SkillType.MAGIC,user, target));
+		GameManager.getManager().getActions().add(new Action(variation, actionType, skillType,user, target));
 	}
 
 }
