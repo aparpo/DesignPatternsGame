a83b18d5104c45b6e90943bf19ec9a2eb129b573
aparpo <alvaro.parpolowicz@live.u-tad.com>
2021-04-28 15:53:39 +0200

    Random Strategy

--- GameDesignPattern/src/strategyPattern/RandomStrategy.java
+++ GameDesignPattern/src/strategyPattern/RandomStrategy.java
@@ -1,5 +1,16 @@
 package strategyPattern;
+import java.util.ArrayList;
+import java.util.List;
 
-public class RandomStrategy {
+import base.Character;
+import decoratorPattern.ActiveItemDecorator;
+
+public class RandomStrategy implements ActionStrategy{
+
+	public void decision(Character user, Character player) {
+		List<ActiveItemDecorator> list = new ArrayList<ActiveItemDecorator>(); 
+		list = user.getEquipment().areThereAnyActives(list); //Conseguir todos los items con habilidades de activas
+		list.get((int) Math.random()* list.size()).useSkill(user, player); //Elegir uno aleatorio y lanzar su habilidad
+	}
 
 }
