ddad0372c56e7dde6896418ade33312e138b23f8
erickdcs <51513004+erickdcs@users.noreply.github.com>
2021-04-28 15:48:54 +0200

    interface Strategy

--- GameDesignPattern/src/strategyPattern/ActionStrategy.java
+++ GameDesignPattern/src/strategyPattern/ActionStrategy.java
@@ -1,5 +1,5 @@
 package strategyPattern;
-
+import base.Character;
 public interface ActionStrategy {
 	public void decision(Character user, Character player);
 }
--- GameDesignPattern/src/strategyPattern/MediumStrategy.java
+++ GameDesignPattern/src/strategyPattern/MediumStrategy.java
@@ -4,4 +4,11 @@ public class MediumStrategy implements ActionStrategy{
 	public void decision(Character user, Character player) {
 		
 	}
+	public boolean worthDefense(Character user, Character player) {
+		boolean retorno;
+		if(true) {
+			
+		}
+		return true;
+	}
 }
