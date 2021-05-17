6cae45afd8b5e929507f5720dacd576a0eac3766
Jorge Tesch <jorge.tesch@users.noreply.github.com>
2021-05-17 20:16:13 +0200

    (*-*)

--- GameDesignPattern/src/decoratorPattern/items/bossWeapons/QuelaagWeapon.java
+++ GameDesignPattern/src/decoratorPattern/items/bossWeapons/QuelaagWeapon.java
@@ -19,15 +19,22 @@ public class QuelaagWeapon extends ActiveItemDecorator{ //Resta un porcentaje de
 		super(equipment, "Chaos Blade", "Black Fire Slash", new Stats(0, 0, 35, 0, 0), ActionType.OFFENSIVE, SkillType.PHYSICAL, Tier.A);
 	}
 
-	@Override
 	public void useSkill(Character user, Character target) {
-		int aux = user.getEquipment().getAttack() - (target.getEquipment().getDefense() / 2); //Resultado del combate
-		if(aux <= 0) { //La defensa es mayor que el ataque
-			aux = 0; //La vida que se resta al objetivo es cero
+		int aux = user.getEquipment().getAttack() - (target.getEquipment().getDefense() / 2); //Resultado del combate, ignorando la mitad de la defensa del objetivo.
+		if(aux <= 0) { //La defensa es mayor que el ataque.
+			aux = 0; //La vida que se resta al objetivo es cero.
+		}		
+		Stats variationTarget = new Stats( -aux, 0, 0, 0, 0); //Actualizar vida actual del objetivo.
+		
+		Stats variationUser;
+		if(target.getEquipment().getLife() > 1 && target.getEquipment().getLife() < (aux / 4)) {
+			variationUser = new Stats( target.getEquipment().getLife() - 1 , 0, 0, 0, 0); //Si el daño es superior a la vida del usuario y este tiene más de 1 de vida, 
+																							//en vez de matarle le deja a 1 de vida.
 		}
 		
-		Stats variationTarget = new Stats( -aux, 0, 0, 0, 0); //Actualizar vida actual del objetivo
-		Stats variationUser = new Stats( -(aux / 4) , 0, 0, 0, 0); //Actualizar vida actual del objetivo
+		else {
+			variationUser = new Stats( -(aux / 4) , 0, 0, 0, 0); //Actualizar vida actual del usuario.
+		}
 		
 		GameManager.getManager().getActions().add(new Action(variationTarget, actionType, skillType, user, target));
 		GameManager.getManager().getActions().add(new Action(variationUser, actionType, skillType, user, user));
