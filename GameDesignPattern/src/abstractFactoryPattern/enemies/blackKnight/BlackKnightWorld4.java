2710895faf84d5353066d67e812acbb5d5d48ab3
aparpo <alvaro.parpolowicz@live.u-tad.com>
2021-05-20 13:29:17 +0200

    Menos items (1)

--- GameDesignPattern/src/abstractFactoryPattern/enemies/blackKnight/BlackKnightWorld4.java
+++ GameDesignPattern/src/abstractFactoryPattern/enemies/blackKnight/BlackKnightWorld4.java
@@ -9,6 +9,5 @@ public class BlackKnightWorld4 extends BlackKnight{
 	public BlackKnightWorld4() {
 		super("Axe Knight");
 		this.equipment = new RegularItem(this.equipment, "Base Equipment", levelStats(blackKnightBaseStats, World.WORLD4));	
-		addItem(new MagicCoat()); //Por defecto en este nivel tienen p.e. capa magica
 	}
 }
