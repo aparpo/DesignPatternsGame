1ec2fcd6ca1cb04e78be4361c591cc67bdbe4881
aparpo <alvaro.parpolowicz@live.u-tad.com>
2021-05-23 21:21:46 +0200

    Change strategy de los bosses

--- GameDesignPattern/src/com/utad/project/abstractFactoryPattern/enemies/bosses/Boss.java
+++ GameDesignPattern/src/com/utad/project/abstractFactoryPattern/enemies/bosses/Boss.java
@@ -1,5 +1,7 @@
 package com.utad.project.abstractFactoryPattern.enemies.bosses;
 import com.utad.project.base.Enemy;
+import com.utad.project.strategyPattern.bossStrategies.HardStrategy;
+import com.utad.project.strategyPattern.bossStrategies.MediumStrategy;
 
 public abstract class Boss extends Enemy{
 	//A diferencia del resto de enemigos, los Bosses no requieren de estadisticas base ya que estos son independientes entre si, 
@@ -7,5 +9,16 @@ public abstract class Boss extends Enemy{
 	
 	public Boss(String bossName, String sprite) {
 		super(bossName, sprite);
+	}
+	
+	@Override
+	public void changeStrategy() {
+		if(this.getEquipment().getLife() < this.getEquipment().getMaxLife()*0.3 && !(this.getBehaviour()instanceof HardStrategy)) {
+			this.setBehaviour(new HardStrategy());
+		}else if(this.getBehaviour() instanceof HardStrategy){
+			if(Math.random()<0.3) {
+				this.setBehaviour(new MediumStrategy());
+			}
+		}
 	}
 }
